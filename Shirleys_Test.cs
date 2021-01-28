5be9f094b320e34517d7481101770ab3bc8eb469
Tapedigo <Tapedigo1996@gmail.com>
2021-01-27 21:52:09 -0500

    added shopping cart file and added to TestNames method

--- Shirleys_Test.cs
+++ Shirleys_Test.cs
@@ -9,6 +9,7 @@ namespace shirley_creations_test
         ProductManager _projectManager = new ProductManager();
         private List<string> _listOfProductsNames = new List<string>();
         private List<double> _listOfProductsPrice = new List<double>();
+        [Fact]
         public void AssignProductsToTestList()
         {
             foreach (var product in _projectManager.DeserializeProducts())
@@ -18,11 +19,17 @@ namespace shirley_creations_test
             }
         }
         [Fact]
-        public void TestName()
+        public void TestNames()
         {
             AssignProductsToTestList();
             string expected = "70s Band T-shirt Quilt";
             Assert.Equal(_listOfProductsNames[0],expected);
+            string expected1 = "Standard Black and Red Quilt";
+            Assert.Equal(_listOfProductsNames[1],expected1);
+            string expected2 = "Superhero T-Shirt Quilt";
+            Assert.Equal(_listOfProductsNames[2],expected2);
+            string expected3 = "Standard Cloth Quilt Themed Mask";
+            Assert.Equal(_listOfProductsNames[3],expected3);
         }
     }
 }
